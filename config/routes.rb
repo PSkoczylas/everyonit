e5367fe303efc7cf565ea987ad89537a10ce255a
Piotr Skoczylas <piotr.skoczylas93@gmail.com>
2022-02-13 22:00:51 +0100

    [Feature-1] Add login to header

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   root 'specialists#index'
 
-  devise_for :specialists
+  devise_for :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
