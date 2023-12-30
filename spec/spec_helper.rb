871465782fd3b311dd9d1c6b0784f3838d9fc453
Piotr Skoczylas <piotr.skoczylas93@gmail.com>
2023-12-30 20:11:24 +0100

    [Feature-2] Merge with main

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -63,6 +63,9 @@ RSpec.configure do |config|
   #   # Limits the available syntax to the non-monkey patched syntax that is
   #   # recommended. For more details, see:
   #   # https://relishapp.com/rspec/rspec-core/docs/configuration/zero-monkey-patching-mode
+  #   #   - http://rspec.info/blog/2012/06/rspecs-new-expectation-syntax/
+  #   #   - http://www.teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+  #   #   - http://rspec.info/blog/2014/05/notable-changes-in-rspec-3/#zero-monkey-patching-mode
   #   config.disable_monkey_patching!
   #
   #   # Many RSpec users commonly either run the entire suite or an individual
